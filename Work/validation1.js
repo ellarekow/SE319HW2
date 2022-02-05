db78a2fc62e99deba56eb0ff566d4d8d104c1335
ellarekow <ellarekow@gmail.com>
2022-02-05 16:44:14 -0600

    Worked on validation

--- Work/validation1.js
+++ Work/validation1.js
@@ -1,14 +1,13 @@
 function validate1() {
-    valCheck = true;
     var fNameResult = nameCheck(document.forms["information"]["fname"].value);
     var lNameResult = nameCheck(document.forms["information"]["lname"].value);
 
 }
 
 function nameCheck(name) {
-    if (name.match("^[a-zA-Z0-9]*$") && name != null) {
+    if (name.match("^[a-zA-Z0-9]*$") && name != null)
         return true;
-    }
-    else
+    else {
         return false;
+    }
 }
