8583f01b38d2a84a11e93d08e50d729e43bf6beb
ellarekow <ellarekow@gmail.com>
2022-02-07 10:33:16 -0600

    Continued work on validation script

--- Work/validation1.js
+++ Work/validation1.js
@@ -1,13 +1,44 @@
 function validate1() {
-    var fNameResult = nameCheck(document.forms["information"]["fname"].value);
-    var lNameResult = nameCheck(document.forms["information"]["lname"].value);
+    const fNameResult = nameCheck(document.forms["information"]["fname"].value);
+    const lNameResult = nameCheck(document.forms["information"]["lname"].value);
 
+    getImage(Boolean(fNameResult), "fname");
+    getImage(Boolean(lNameResult), "lname");
+
+    getNotification(Boolean(fNameResult), "fname");
+    getNotification(Boolean(lNameResult), "lname");
 }
 
 function nameCheck(name) {
-    if (name.match("^[a-zA-Z0-9]*$") && name != null)
-        return true;
-    else {
-        return false;
+    console.log("name:", name);
+    return (name.match(/^[a-zA-Z0-9]*$/) && name != null || name != "");
+}
+
+function dropCheck(value) {
+    return value != "blank";
+}
+
+function getNotification(bool, id) {
+    var label = document.getElementById("labelNotify" + id);
+    if (label == null) {
+        label = document.createElement("LABEL");
+        label.id = "labelNotify" + id;
+        label.setAttribute('class', 'errorMessage');
     }
+
+    const errors = { fname: "Please enter a valid name", lname: "Please enter a valid name", gender: "Please select an option", state: "Please select an option" }
+    errors[id]
+
+    label.innerHTML = bool ? "" : errors[id];
+    document.getElementById(id).appendChild(label);
+}
+
+function getImage(bool, id) {
+    var image = document.getElementById("image" + id);
+    if (image == null) {
+        image = new Image(15, 15);
+        image.id = "image" + id;
+    }
+    image.src = bool ? './correct.png' : './wrong.png';
+    document.getElementById(id).appendChild(image);
 }
